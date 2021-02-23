5cbf60241cf722973de84178400a1bb951309d72
Wen Ya <wenya@vcinema.cn>
2021-02-23 23:22:13 +0800

    init

---
+++ out.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+exports[Symbol.toStringTag] = 'Module';
+
+var Symbol$1 = "Hello World";
+
+exports.Symbol = Symbol$1;
